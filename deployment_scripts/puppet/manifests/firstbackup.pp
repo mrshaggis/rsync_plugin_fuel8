e2857380de6e57b98de2ec524a1134b67a88f377
mrshaggis <mrshaggis@localhost.com>
2016-08-18 12:58:33 +0200

    added puppet manifest for the first backup

---
+++ deployment_scripts/puppet/manifests/firstbackup.pp
@@ -0,0 +1,10 @@
+$nodes_hash = hiera('nodes')
+
+$rsyn_node = filter_nodes($nodes_hash, 'role', 'rsync-fuel-plugin8')
+
+$rsync_nodename = $::rsync_node[0]['fqdn']
+
+cron {'rsynb-backup':
+  command => "/usr/bin/rsync -az /etc ${rsync_nodename}::backup/$(hostname)/",
+  user    => 'root',
+}
