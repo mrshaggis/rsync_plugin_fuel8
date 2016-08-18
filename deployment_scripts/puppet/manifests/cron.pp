25aac6a7ac7ee73cacc160442c6bc47cf77af76a
mrshaggis <mrshaggis@localhost.com>
2016-08-18 11:11:23 +0200

    added in

--- deployment_scripts/puppet/manifests/cron.pp
+++ deployment_scripts/puppet/manifests/cron.pp
@@ -1,5 +1,11 @@
+$nodes_hash = hiera('nodes')
+
+$rsync_node = filter_nodes($nodes_hash, 'role','rsync-plugin-fuel8')
+
+$rsync_nodename = $::rsync_node[0]['fqdn']
+
 cron {'rsync-backup':
-  command => "/usr/bin/rsync -az /etc ${mountpoint}",
+  command => "/usr/bin/rsync -az /etc ${rsync_nodename}::backup/$(hostname)/",
   user    => 'root',
   hour    => 4,
   minute  => 0,
