67eae8db967c2b28d5190cfe4be73c462e57a55c
mrshaggis <mrshaggis@localhost.com>
2016-08-26 14:26:31 +0200

    removed the :: from infront of rsync_node[0]...

--- deployment_scripts/puppet/manifests/cron.pp
+++ deployment_scripts/puppet/manifests/cron.pp
@@ -2,7 +2,7 @@ $nodes_hash = hiera('nodes')
 
 $rsync_node = filter_nodes($nodes_hash, 'role','rsync-plugin-fuel8')
 
-$rsync_nodename = $::rsync_node[0]['fqdn']
+$rsync_nodename = $rsync_node[0]['fqdn']
 
 cron {'rsync-backup':
   command => "/usr/bin/rsync -az /etc ${rsync_nodename}::backup/$(hostname)/",
--- deployment_scripts/puppet/manifests/firstbackup.pp
+++ deployment_scripts/puppet/manifests/firstbackup.pp
@@ -2,7 +2,7 @@ $nodes_hash = hiera('nodes')
 
 $rsync_node = filter_nodes($nodes_hash, 'role', 'rsync-fuel-plugin8')
 
-$rsync_nodename = $::rsync_node[0]['fqdn']
+$rsync_nodename = $rsync_node[0]['fqdn']
 
 cron {'rsynb-backup':
   command => "/usr/bin/rsync -az /etc ${rsync_nodename}::backup/$(hostname)/",
